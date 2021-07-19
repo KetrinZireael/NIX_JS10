1a68fccb1272c7114e65539319ec78c6c7470a0b
Ketrin <geleczkak26@gmail.com>
2021-07-19 14:45:08 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,10 @@
-// function arrayName(name) {
-//    const unwantedNames = ['Дима', 'Саша', 'Ольга', 'Никита'];
-//    let resName = [];
-
-//    for (let i = 0; i < name.length; i++) {
-//       if(unwantedNames[i] === name[i]) {
-//          name.splice(i, 1);
-//       }   
-//    }
-   
-//    return name;
-// }
-
-// console.log(arrayName(['Никита', 'Саша', 'Анастасия', 'Дима', 'Катя', 'Лера']));
-
-
 const unwantedNames = ['Дима', 'Саша', 'Ольга', 'Никита'];
 let lastName = ['Никита', 'Саша', 'Анастасия', 'Дима', 'Катя', 'Лера'];
 
-let pos = lastName.indexOf('Дима');
-console.log(pos);
-
-let remove = lastName.splice(pos, 1);
-
+for (let i = 0; i < unwantedNames.length; i++) {
+    let pos = lastName.indexOf(unwantedNames[i]);
+    if (pos != -1) {
+        lastName.splice(pos, 1);
+    }
+}
 console.log(lastName);
-
